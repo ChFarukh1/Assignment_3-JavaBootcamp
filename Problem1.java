d37f3bf9484bb19c62237654841a88f47f931846
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-08 14:53:51 +0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class CricketScorecardTask1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name of Team 1: ");
+        String team1 = scanner.nextLine();
+        System.out.print("Enter name of Team 2: ");
+        String team2 = scanner.nextLine();
+
+      
+        Random random = new Random();
+        boolean team1BatsFirst = random.nextBoolean();
+
+        
+        if (team1BatsFirst) {
+            System.out.println(team1 + " won the toss and will bat first.");
+        } else {
+            System.out.println(team2 + " won the toss and will bat first.");
+        }
+    }
+}
 
